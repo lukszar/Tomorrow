2314bea7f15f83d3a2bebc4fb33ac8389046c637
Łukasz Szarkowicz <szarkowicz@gmail.com>
2022-10-17 22:54:08 +0200

    Date interval components and descriptions

---
+++ Sources/DateInterval+Components.swift
@@ -0,0 +1,67 @@
+//
+//  DateInterval+Components.swift
+//  Tomorrow
+//
+//  Created by Łukasz Szarkowicz on 17/10/2022.
+//  Copyright © 2022 Mobilee. All rights reserved.
+//
+
+import Foundation
+
+@available(iOS 10.0, tvOSApplicationExtension 10.0, tvOS 10.0, watchOSApplicationExtension 3.0, OSXApplicationExtension 10.12, macOS 10.12, *)
+extension DateInterval {
+    
+    /**
+     Calculates duration using given components.
+     
+     F.ex. if difference between dates is 26h and you calculate duration using .day and .hour components it will return 1 day and 2h.
+     
+     - parameter components: Components which will be considered to return results for.
+     
+     - returns: Duration between given dates in format of DateComponents
+     */
+    func duration(representedBy components: Set<Calendar.Component>) -> DateComponents {
+        return Calendar.current.dateComponents(components, from: start, to: end)
+    }
+    
+    /**
+     Calculates duration using given one component.
+     
+     - parameter component: Component which will be considered to return results for.
+     
+     - returns: Duration between given dates in format of DateComponents
+     */
+    func duration(representedBy component: Calendar.Component) -> DateComponents {
+        let comp: Set<Calendar.Component> = [component]
+        return Calendar.current.dateComponents(comp, from: start, to: end)
+    }
+    
+    func inYears() -> Int {
+        duration(representedBy: .year).year ?? 0
+    }
+    
+    func inMonths() -> Int {
+        duration(representedBy: .month).month ?? 0
+    }
+    
+    func inDays() -> Int {
+        duration(representedBy: .day).day ?? 0
+    }
+    
+    func inHours() -> Int {
+        duration(representedBy: .hour).hour ?? 0
+    }
+    
+    func inMinutes() -> Int {
+        duration(representedBy: .minute).minute ?? 0
+    }
+    
+    func inSeconds() -> Int {
+        duration(representedBy: .second).second ?? 0
+    }
+    
+    @available(iOS 15.0, tvOSApplicationExtension 15.0, tvOS 15.0, watchOSApplicationExtension 8.0, OSXApplicationExtension 12.0, macOS 12.0, *)
+    func shortDescription() -> String {
+        return (start..<end).formatted(.components(style: .narrow, fields: [.year, .month, .day, .hour, .minute, .second]))
+    }
+}
---
+++ Tests/DateIntervalComponentTests.swift
@@ -0,0 +1,76 @@
+//
+//  DateIntervalComponentTests.swift
+//  Tomorrow-iOSTests
+//
+//  Created by Łukasz Szarkowicz on 17/10/2022.
+//  Copyright © 2022 Mobilee. All rights reserved.
+//
+
+import XCTest
+@testable import Tomorrow
+
+@available(iOS 10.0, tvOSApplicationExtension 1.0, tvOS 1.0, watchOSApplicationExtension 1.0, OSXApplicationExtension 10.1, macOS 10.1, *)
+class DateIntervalComponentTests: XCTestCase {
+    
+    var startDate: Date!
+    var endDate: Date!
+    var interval: DateInterval!
+    var calendar: Calendar!
+    
+    override func setUp() {
+        calendar = Calendar(identifier: .gregorian)
+        startDate = Tomorrow.date(from: "2022-07-15T19:30:00Z")
+        endDate = Tomorrow.date(from: "2022-07-16T21:00:00Z")
+        
+        interval = DateInterval(start: startDate, end: endDate)
+       
+    }
+
+    override func setUpWithError() throws {
+
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testYears() {
+        let years = interval.inYears()
+        XCTAssertEqual(years, 0)
+    }
+    
+    func testMonths() {
+        let months = interval.inMonths()
+        XCTAssertEqual(months, 0)
+    }
+    
+    func testDays() {
+        let days = interval.inDays()
+        XCTAssertEqual(days, 1)
+    }
+    
+    func testHours() {
+        let hours = interval.inHours()
+        XCTAssertEqual(hours, 25)
+    }
+    
+    func testMinutes() {
+        let minutes = interval.inMinutes()
+        XCTAssertEqual(minutes, 1530)
+    }
+    
+    func testDuration() {
+        let comps = interval.duration(representedBy: [.day, .hour])
+        
+        XCTAssertEqual(comps.day, 1)
+        XCTAssertEqual(comps.hour, 1)
+    }
+    
+    @available(iOS 15.0, tvOSApplicationExtension 15.0, tvOS 15.0, watchOSApplicationExtension 8.0, OSXApplicationExtension 12.0, macOS 12.0, *)
+    func testShortDescription() {
+        let desc = interval.shortDescription()
+        XCTAssertEqual(desc, "1 doba 1godz. 30min")
+    }
+
+}
